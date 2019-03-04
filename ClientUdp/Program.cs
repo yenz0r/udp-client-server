af9d4474bde304fa6ded3e65b58065d2cdf9a128
yenz0redd <375293774737@yandex.ru>
2019-03-04 19:57:32 +0300

    base for project

---
+++ ClientUdp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ClientUdp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8081;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while (true)
+            {
+                var buffer = new byte[255];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                } while (udpSocket.Available > 0);
+
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Done!"), senderEndPoint);
+
+                //udpSocket.Shutdown(SocketShutdown.Both);
+                //udpSocket.Close();
+
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ ServerUdp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ServerUdp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8082;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while (true)
+            {
+                Console.WriteLine("input message : ");
+                var message = Console.ReadLine();
+
+                var serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                udpSocket.Send(Encoding.UTF8.GetBytes(message));
+
+                var buffer = new byte[255];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                } while (udpSocket.Available > 0);
+
+                Console.WriteLine("answer from client : ", data);
+            }
+        }
+    }
+}
